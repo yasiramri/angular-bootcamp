9af60a4d256c4ea3ed3d818bffe70c5abd41c4b1
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-02-05 15:48:32 +0700

    update routing ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './template/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent },
+  {
+    path: 'pages',
+    loadChildren: () =>
+      import('./pages/pages.module').then((m) => m.PagesModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
