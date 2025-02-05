666e0438ae765a26830874dcbdd347e1f23e1044
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-02-05 15:46:08 +0700

    update todo service

--- src/app/pages/services/todo.service.ts
+++ src/app/pages/services/todo.service.ts
@@ -10,7 +10,23 @@ export class TodoService {
 
   constructor(private http: HttpClient) {}
 
+  // Get all todos
   getTodos(): Observable<any> {
     return this.http.get(this.apiUrl + '/todos');
+  }
+
+  // Add a new todo
+  addTodo(todo: any): Observable<any> {
+    return this.http.post(this.apiUrl + '/todos', todo);
+  }
+
+  // Update an existing todo
+  updateTodo(id: number, todo: any): Observable<any> {
+    return this.http.put(this.apiUrl + '/todos/' + id, todo);
+  }
+
+  // Delete a todo by ID
+  deleteTodo(id: number): Observable<any> {
+    return this.http.delete(this.apiUrl + '/todos/' + id);
   }
 }
