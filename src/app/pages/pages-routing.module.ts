a919ba538f28d6c00a84c42f4bbfe64a9a67f3a0
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-02-06 16:10:54 +0700

    update routing

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { CategoryComponent } from './category/category.component';
+import { EditTodoComponent } from './edit-todo/edit-todo.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'category',
     component: CategoryComponent,
+  },
+  {
+    path: 'edit/:id',
+    component: EditTodoComponent,
   },
 ];
 
