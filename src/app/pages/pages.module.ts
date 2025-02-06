f32a89d7da36d446c0a9f53ecfedcf4045df6e91
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-02-06 11:46:05 +0700

    add some module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { TodosComponent } from './todos/todos.component';
 import { CategoryComponent } from './category/category.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TodosComponent, CategoryComponent],
-  imports: [CommonModule, PagesRoutingModule, MatTableModule],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    MatTableModule,
+    NgbModule,
+    ReactiveFormsModule,
+    NgSelectModule,
+    FormsModule,
+  ],
 })
 export class PagesModule {}
