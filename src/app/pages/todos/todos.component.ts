dfee225430af59beeaee7e94d7fa5c764e1cf77d
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-02-06 11:44:40 +0700

    add form for upload todo

--- src/app/pages/todos/todos.component.ts
+++ src/app/pages/todos/todos.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { CategoryService } from '../services/category.service';
 
 export interface Todos {
   id: number; // Assuming you have an ID for each todo
@@ -9,6 +16,10 @@ export interface Todos {
   progress: number;
 }
 
+export interface Category {
+  id: number;
+  category: string;
+}
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
@@ -17,6 +28,7 @@ export interface Todos {
 })
 export class TodosComponent implements OnInit {
   userData: Todos[] = [];
+  categoriesList: Category[] = [];
   displayedColumns: string[] = [
     'title',
     'description',
@@ -25,10 +37,25 @@ export class TodosComponent implements OnInit {
     'actions',
   ];
 
-  constructor(private todoService: TodoService) {}
+  modalAdd: any;
+
+  constructor(
+    private todoService: TodoService,
+    private categoryService: CategoryService,
+    private modalService: NgbModal,
+    private formBuilder: UntypedFormBuilder,
+    private restApiService: TodoService
+  ) {}
 
   ngOnInit(): void {
     this.getTodos();
+    this.getCategory();
+    this.addForm = this.formBuilder.group({
+      title: [null, [Validators.required]],
+      category_id: [null],
+      description: [null],
+      progress: [null],
+    });
   }
 
   // Fetch the todos from the backend
@@ -42,6 +69,18 @@ export class TodosComponent implements OnInit {
         console.error('Error fetching todos', error);
       }
     );
+  }
+
+  private getCategory() {
+    this.categoryService.getCategory().subscribe({
+      next: (data) => {
+        this.categoriesList = data.data; // 🔥 Simpan kategori ke array
+        console.log('Categories:', this.categoriesList);
+      },
+      error: (error) => {
+        console.error('Error fetching categories:', error);
+      },
+    });
   }
 
   // Update a todo item
@@ -76,6 +115,57 @@ export class TodosComponent implements OnInit {
           console.error('Error deleting todo', error);
         }
       );
+    }
+  }
+
+  addButton(content: any) {
+    this.modalService.open(content, { size: 'xl', centered: true });
+  }
+
+  addForm!: UntypedFormGroup;
+  get af() {
+    return this.addForm.controls;
+  }
+
+  onSubmit() {
+    this.addForm.markAllAsTouched();
+
+    if (this.addForm.valid) {
+      let requestData = this.addForm.value;
+      console.log(this.addForm.value);
+      const formData = this.addForm.value;
+      formData.category_id = parseInt(formData.category_id, 10);
+      formData.progress = parseInt(formData.progress, 10);
+      console.log(formData);
+      this.todoService.createData(this.addForm.value).subscribe((data: any) => {
+        this.modalService.dismissAll();
+        this.ngOnInit();
+      });
+      this.restApiService.createData(requestData).subscribe({
+        next: (data: any) => {
+          console.log('Response dari backend:', data);
+          this.addForm.reset();
+          this.modalService.dismissAll();
+          this.getTodos();
+        },
+        error: (error: any) => {
+          console.error('Terjadi error:', error);
+        },
+      });
+    }
+  }
+
+  deleteUser(userId: number) {
+    if (confirm('Are you sure you want to delete this user?')) {
+      this.restApiService.deleteTodo(userId).subscribe({
+        next: () => {
+          console.log('User deleted successfully');
+          this.getTodos(); // Refresh Data
+        },
+        error: (error) => {
+          console.error('Error deleting user:', error);
+        },
+      });
     }
   }
 }
